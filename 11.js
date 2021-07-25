9213195794ef00aea1eeb5ccedd286f3289b02aa
zhenyanenado <70278943+zhenyanenado@users.noreply.github.com>
2021-07-25 11:47:17 +0300

    Add files via upload

---
+++ 11.js
@@ -0,0 +1,105 @@
+let a = [];
+let b = ['grid1', 'grid2', 'grid3', 'grid4'];
+let c = document.getElementById('play');
+
+for(let i = 1; i<=4; i++){
+	a.push(document.getElementById('grid'+i));
+
+	a[i-1].addEventListener('touchmove', function(event){
+
+		event = event || window.event;
+
+		let touches = event.touches[0].clientY;
+		let touchesX = event.touches[0].clientX;
+
+			a[0].style.top = touches + 'px';
+			a[0].style.left = touchesX + 'px';
+
+				if(touches >= 350){
+					this.style.width = 150 + 'px';
+					this.style.height = 150 + 'px';
+				}
+
+	})
+
+
+		a[i-1].addEventListener('touchstart', function(event){
+
+			
+			
+			
+
+
+			this.style.width = '350px';
+			this.style.height = '350px';
+
+			c.style.opacity = 1;
+
+
+			if(this.id == 'grid1'){
+				for(let i = 0; i<4; i++){
+					if(i != 0){
+						a[i].style.marginLeft = '300px';
+
+						a[i].style.width = '150px';
+						a[i].style.height = '150px';
+
+						this.style.marginLeft = '0px';
+						c.style.marginLeft = '0px';
+
+					}
+				}
+			}
+
+			if(this.id == 'grid2'){
+				for(let i = 0; i<4; i++){
+					if(i !=1){
+						a[0].style.marginLeft = '500px';
+						a[2].style.marginLeft = '350px';
+						a[3].style.marginLeft = '300px';
+
+						a[i].style.width = '150px';
+						a[i].style.height = '150px';
+
+						this.style.marginLeft = '-150px';
+						c.style.marginLeft = '0px';
+					}
+				}
+			}
+
+			if(this.id == 'grid3'){
+				for(let i = 0; i<4; i++){
+					if(i !=2){
+						a[0].style.marginLeft = '0px';
+						a[1].style.marginLeft = '-50px';
+						a[3].style.marginLeft = '-200px';
+
+						a[i].style.width = '150px';
+						a[i].style.height = '150px';
+
+						this.style.marginLeft = '250px';
+						c.style.marginLeft = '550px';
+
+					}
+				}
+			}
+
+			if(this.id == 'grid4'){
+				for(let i = 0; i<4; i++){
+					if(i !=3){
+						a[i].style.marginLeft = '50px';
+
+						this.style.marginLeft = '150px';
+						c.style.marginLeft = '550px';
+
+						a[i].style.width = '150px';
+						a[i].style.height = '150px';
+					}
+				}
+			}
+
+		})
+			
+
+}
+
